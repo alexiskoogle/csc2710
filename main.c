15f8949776e69d8abfdbeef185f8d0986af84aff
alexiskoogle <130502589+alexiskoogle@users.noreply.github.com>
2023-10-03 11:57:54 -0400

    Update main.c

--- main.c
+++ main.c
@@ -29,6 +29,12 @@ void bubbleSort(int [], int);
 void heapify(int[], int, int);
 void heapSort(int[], int );
 
+int counter_ms = 0;
+int counter_qs = 0;
+int counter_is = 0;
+int counter_ss = 0;
+int counter_bs = 0;
+int counter_hs = 0;
 
 int main(){
 	int x,y;
@@ -61,6 +67,7 @@ int main(){
 	clock_t end = clock();
 	double time = (double) (end-start) / CLOCKS_PER_SEC * 10.0;
 	cout << "Selection Sort" << endl;
+	cout << "Compares:" << counter_ss << endl;
 	cout << "Time:" << time << endl;
 	for (int i = 0; i < x; i++)
 		{
@@ -74,6 +81,7 @@ int main(){
 	clock_t end2 = clock();
 	double time2 = (double) (end2-start2) / CLOCKS_PER_SEC * 10.0;
 	cout << "Merge Sort" << endl;
+	cout << "Compares:" << counter_ms << endl;
 	cout << "Time:" << time2 << endl;
 	for (int i = 0; i < x; i++)
 		{
@@ -87,6 +95,7 @@ int main(){
 	clock_t end3 = clock();
 	double time3 = (double) (end3-start3) / CLOCKS_PER_SEC * 10.0;
 	cout << "Insertion Sort" << endl;
+	cout << "Compares:" << counter_is << endl;
 	cout << "Time:" << time3 << endl;
 	for (int i = 0; i < x; i++)
 		{
@@ -100,6 +109,7 @@ int main(){
 	clock_t end4 = clock();
 	double time4 = (double) (end4-start4) / CLOCKS_PER_SEC * 10.0;
 	cout << "Quick Sort" << endl;
+	cout << "Compares:" << counter_qs << endl;
 	cout << "Time:" << time4 << endl;
 	for (int i = 0; i < x; i++)
 		{
@@ -113,6 +123,7 @@ int main(){
 	clock_t end5 = clock();
 	double time5 = (double) (end5-start5) / CLOCKS_PER_SEC * 10.0;
 	cout << "Bubble Sort" << endl;
+	cout << "Compares:" << counter_bs << endl;
 	cout << "Time:" << time5 << endl;
 	for (int i = 0; i < x; i++)
 		{
@@ -126,6 +137,7 @@ int main(){
 	clock_t end6 = clock();
 	double time6 = (double) (end6-start6) / CLOCKS_PER_SEC * 10.0;
 	cout << "Heap Sort" << endl;
+	cout << "Compares:" << counter_hs << endl;
 	cout << "Time:" << time6 << endl;
 	for (int i = 0; i < x; i++)
 		{
@@ -180,6 +192,7 @@ void merge(int numbers[], int first, int middle, int last) {
 			j++;
 		}
 		k++;
+		counter_ms++;
 	}
 	//Copy the remaining elements of left the right away in case the arrays are uneven
 	while (i < l1) {
@@ -216,6 +229,7 @@ void partition(int s[], int low, int high, int& pivotpoint)
 	j = low;
 	for(i = low + 1; i <= high; i++)
 		{
+			counter_qs++;
 			if (s[i] < pivotitem)
 				{
 					j++;
@@ -240,6 +254,7 @@ void insertionSort(int n, int S[])
 			j = i-1;
 			while (j>=0 && S[j] > x)
 				{
+					counter_is++;
 					S[j+1] = S[j];
 					j--;
 				}
@@ -259,6 +274,7 @@ void selectionSort(int x, int random[])
 			
 			for (j = i + 1; j < x; j++)
 				{
+					counter_ss++;
 					if (random[j] < random[small])
 						{
 							small = j;
@@ -289,6 +305,7 @@ void bubbleSort(int arr[], int n){
 		swapped=false;
 		
 		for (int j=0; j < n-i-1; j++){
+			counter_bs++;
 			if (arr[j] > arr[j+1]){
 				swap(arr[j], arr[j+1]);
 				swapped = true;
@@ -304,10 +321,12 @@ void heapify(int arr[], int n, int i) {
 	int largest = i; 
 	int left = 2*i+1; 
 	int right = 2*i+2; 
-	
+
+	counter_hs++;
 	if (left < n && arr[left] > arr[largest])
 		largest= left;
-	
+
+	counter_hs++;
 	if (right < n && arr[right] > arr[largest])
 		largest= right;
 	
